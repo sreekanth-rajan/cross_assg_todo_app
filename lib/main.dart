7ba2b05bd12c9abde0799983920ca7fd0bd750c9
sreekanth-rajan <sreekanthrajan321@gmail.com>
2024-05-09 23:46:22 +0530

    updated user  login and reg

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 import 'login_page.dart'; // Import your login page file
+import 'reg_page.dart'; // Import your registration page file
 import 'todo_page.dart';
 
 void main() async {
@@ -25,160 +26,43 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      home: HomePage(), // Ensure HomePage is defined
       routes: {
         '/login_page': (context) => LoginPage(), // Define route for login page
+        '/reg_page': (context) => RegistrationPage(), // Define route for registration page
         '/todo': (context) => TodoPage(),
       },
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final controllerUsername = TextEditingController();
-  final controllerPassword = TextEditingController();
-  final controllerEmail = TextEditingController();
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('To-Do App'),
+        title: const Text('Home Page'),
       ),
       body: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(8),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Center(
-                child: const Text('User Registration',
-                    style:
-                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              Center(
-                child: const Text('Enter your details',
-                    style: TextStyle(fontSize: 16)),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              TextField(
-                controller: controllerUsername,
-                keyboardType: TextInputType.text,
-                textCapitalization: TextCapitalization.none,
-                autocorrect: false,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black)),
-                    labelText: 'Username'),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              TextField(
-                controller: controllerEmail,
-                keyboardType: TextInputType.emailAddress,
-                textCapitalization: TextCapitalization.none,
-                autocorrect: false,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black)),
-                    labelText: 'E-mail'),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              TextField(
-                controller: controllerPassword,
-                obscureText: true,
-                keyboardType: TextInputType.text,
-                textCapitalization: TextCapitalization.none,
-                autocorrect: false,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black)),
-                    labelText: 'Password'),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Container(
-                height: 50,
-                child: TextButton(
-                  child: const Text('Sign Up'),
-                  onPressed: () => doUserRegistration(),
-                ),
-              )
-            ],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login_page');
+              },
+              child: const Text('Login'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/reg_page');
+              },
+              child: const Text('Sign Up'),
+            ),
+          ],
         ),
       ),
     );
-  }
-
-  void showSuccess(String message) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Success!"),
-          content: Text(message),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                Navigator.pushNamed(context, '/login_page'); // Navigate to login page
-              },
-              child: Text("OK"),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void showError(String errorMessage) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Error!"),
-          content: Text(errorMessage),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text("OK"),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void doUserRegistration() async {
-    final username = controllerUsername.text.trim();
-    final email = controllerEmail.text.trim();
-    final password = controllerPassword.text.trim();
-
-    final user = ParseUser.createUser(username, password, email);
-
-    var response = await user.signUp();
-
-    if (response.success) {
-      showSuccess("User was successfully created!"); // Pass the success message
-    } else {
-      showError(response.error!.message);
-    }
   }
 }
---
+++ lib/reg_page.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
+
+class RegistrationPage extends StatefulWidget {
+  @override
+  _RegistrationPageState createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  final controllerUsername = TextEditingController();
+  final controllerPassword = TextEditingController();
+  final controllerEmail = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('To-Do App - User Registration'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Center(
+                child: const Text('User Registration',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Center(
+                child: const Text('Enter your details',
+                    style: TextStyle(fontSize: 16)),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              TextField(
+                controller: controllerUsername,
+                keyboardType: TextInputType.text,
+                textCapitalization: TextCapitalization.none,
+                autocorrect: false,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black)),
+                    labelText: 'Username'),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              TextField(
+                controller: controllerEmail,
+                keyboardType: TextInputType.emailAddress,
+                textCapitalization: TextCapitalization.none,
+                autocorrect: false,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black)),
+                    labelText: 'E-mail'),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              TextField(
+                controller: controllerPassword,
+                obscureText: true,
+                keyboardType: TextInputType.text,
+                textCapitalization: TextCapitalization.none,
+                autocorrect: false,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black)),
+                    labelText: 'Password'),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Container(
+                height: 50,
+                child: TextButton(
+                  child: const Text('Sign Up'),
+                  onPressed: () => doUserRegistration(),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void showSuccess(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Success!"),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.pushNamed(context, '/login_page'); // Navigate to login page
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showError(String errorMessage) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Error!"),
+          content: Text(errorMessage),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void doUserRegistration() async {
+    final username = controllerUsername.text.trim();
+    final email = controllerEmail.text.trim();
+    final password = controllerPassword.text.trim();
+
+    final user = ParseUser.createUser(username, password, email);
+
+    var response = await user.signUp();
+
+    if (response.success) {
+      showSuccess("User was successfully created!"); // Pass the success message
+    } else {
+      showError(response.error!.message);
+    }
+  }
+}
